3b3877b5e1791a56a790536423934b53e0059d92
vishvakbandi <vishvakbandi@gmail.com>
2021-03-17 13:22:40 -0500

    clean up search.js

--- src/components/search.js
+++ src/components/search.js
@@ -80,33 +80,6 @@ function Search() {
         // console.log(destinationPlaceId);
     }
 
-    async function getPlaces(location) {
-        const reqOptions = {
-            method: "GET",
-            headers: {
-                "x-rapidapi-key": `${process.env.REACT_APP_SKYSCANNER_KEY}`,
-                "x-rapidapi-host":
-                    "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-                useQueryString: true,
-            },
-        };
-
-        try {
-            let response = await fetch(
-                "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?" +
-                    new URLSearchParams({ query: location }),
-                reqOptions
-            );
-
-            response = await response.json();
-
-            return { status: 200, data: response.Places[0].PlaceId };
-        } catch (err) {
-            console.log(err);
-            return err;
-        }
-    }
-
     async function getFlightsWithDate() {
         const reqOptions = {
             method: "GET",
