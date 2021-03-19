6db077a2baaf02d6143e642b609701abcd3f77cb
vishvakbandi <vishvakbandi@gmail.com>
2021-03-19 03:00:32 -0500

    syntaax changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../css/Footer.css";
 
-function Footer(props) {
+const Footer = (props) => {
     return (
         <div className="footerTitle">
             <div>{props.title}</div>
             <div className="footerNote">{props.footnote}</div>
         </div>
     );
-}
+};
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../css/Header.css";
 
-function Header(props) {
+const Header = (props) => {
     return <div className="header">{props.title}</div>;
-}
+};
 
 export default Header;
