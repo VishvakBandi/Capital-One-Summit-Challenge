c46366074408b668050771b024791ebe114bdcc9
vishvakbandi <vishvakbandi@gmail.com>
2021-03-19 21:04:22 -0500

    remove unessesary files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import Search from "./components/search";
+import Search from "./components/Search";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
